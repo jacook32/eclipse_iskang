45a5990c09deeeac6a6357957ffe396cdc806079
wku <wku@192.168.172.2>
2020-11-03 10:11:39 +0900

    create 학과

---
+++ Hello World/src/first.java
@@ -0,0 +1,13 @@
+
+public class first {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("sung jin");
+		System.out.println("컴퓨터소프트웨어공학과");
+		
+		
+
+	}
+
+}
