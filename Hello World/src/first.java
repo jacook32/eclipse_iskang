17031c5b7bd4ee872db8641b7a6bbf5980782a8a
wku <wku@192.168.172.2>
2020-11-03 10:16:22 +0900

    create 학번

--- Hello World/src/first.java
+++ Hello World/src/first.java
@@ -4,7 +4,7 @@ public class first {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("sung jin");
-		System.out.println("컴퓨터소프트웨어공학과");
+		System.out.println("20173176");
 		
 		
 
